b032b9e5007b604e31c4508fc568b937483a1d53
ulcorn <legoroller@gmail.com>
2022-12-10 21:39:43 +0300

    jgdyjkdkhdk

--- PythonProject/pages/Analysis.py
+++ PythonProject/pages/Analysis.py
@@ -7,8 +7,23 @@ st.header('Analysis of the statistics')
 
 st.write('Here i would like to analyse the obtained data')
 
-from PythonProject import main
-from main import showpie
+def showbar(dataframe, reasonwhy, color):
+    years = []
+    deathsforreasons = []
+    for i in range(1990, 2020):
+        deathsforreasons.append(dataframe[dataframe['Year'] == i][reasonwhy].sum())
+        years.append(i)
+    fig = px.bar(dataframe[reasonwhy], years, deathsforreasons, labels={'x':'Year', 'y':'Deaths'}, color_discrete_sequence = [color]*len(dataframe))
+    st.plotly_chart(fig)
+def showpie(df, when):
+    names = []
+    values = []
+    for i in list(df)[3:-1]:
+        names.append(i)
+        values.append(df[df['Year'] == when][i].sum())
+    fig = px.pie(df, values=values, names= names, labels={'names':'Reason', 'values':'Deaths'})
+    st.plotly_chart(fig)
+
 
 dfold = pd.read_csv('PythonProject/age-between-15-and-49.csv')
 dfold = dfold.dropna()
@@ -55,7 +70,7 @@ dfveryold = dfveryold[dfveryold['Country'].str.contains('World Bank Lower Middle
 
 st.header('AIDS epidemic')
 reasonwhy = 'HIV/AIDS'
-st.write('There was a pandemic')
+st.write('There was a AIDS pandemic')
 showbar(dfold, reasonwhy, '#00CC96')
 showbar(dfyoung, reasonwhy, '#AB63FA')
 st.write('Stastistics of death on each year')
