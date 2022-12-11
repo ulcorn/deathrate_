198f59947202d9f74596f9e92a672df40bc7f301
ulcorn <120196780+ulcorn@users.noreply.github.com>
2022-12-11 17:40:06 +0300

    Update main.py

--- PythonProjectmilo/main.py
+++ PythonProjectmilo/main.py
@@ -23,7 +23,7 @@ def showpie(df, when):
     fig = px.pie(df, values=values, names= names, labels={'names':'Reason', 'values':'Deaths'})
     st.plotly_chart(fig)
 
-dfold = pd.read_csv('/Users/dreamer/PycharmProjects/programst/PythonProjectmilo/DataFiles/age-between-15-and-49.csv')
+dfold = pd.read_csv('PythonProjectmilo/DataFiles/age-between-15-and-49.csv')
 dfold = dfold.dropna()
 dfold['Year'].astype('object')
 dfold['total_deaths'] = dfold.sum(axis=1, numeric_only=True)
@@ -33,7 +33,7 @@ dfold = dfold[dfold['Country'].str.contains('World Bank Upper Middle Income') ==
 dfold = dfold[dfold['Country'].str.contains('World Bank Lower Middle Income') == False]
 
 
-dfyoung = pd.read_csv('/Users/dreamer/PycharmProjects/programst/PythonProjectmilo/DataFiles/age-between-5-and-14.csv')
+dfyoung = pd.read_csv('PythonProjectmilo/DataFiles/age-between-5-and-14.csv')
 dfyoung = dfyoung.dropna()
 dfyoung['Year'].astype('object')
 dfyoung['total_deaths'] = dfyoung.sum(axis=1, numeric_only=True)
@@ -42,7 +42,7 @@ dfyoung = dfyoung[dfyoung['Country'].str.contains('World') == False]
 dfyoung = dfyoung[dfyoung['Country'].str.contains('World Bank Upper Middle Income') == False]
 dfyoung = dfyoung[dfyoung['Country'].str.contains('World Bank Lower Middle Income') == False]
 
-dftodlers = pd.read_csv('/Users/dreamer/PycharmProjects/programst/PythonProjectmilo/DataFiles/under-age-5.csv')
+dftodlers = pd.read_csv('PythonProjectmilo/DataFiles/under-age-5.csv')
 dftodlers = dftodlers.dropna()
 dftodlers['Year'].astype('object')
 dftodlers['total_deaths'] = dftodlers.sum(axis=1, numeric_only=True)
@@ -51,7 +51,7 @@ dftodlers = dftodlers[dftodlers['Country'].str.contains('World') == False]
 dftodlers = dftodlers[dftodlers['Country'].str.contains('World Bank Upper Middle Income') == False]
 dftodlers = dftodlers[dftodlers['Country'].str.contains('World Bank Lower Middle Income') == False]
 
-dfveryold = pd.read_csv('/Users/dreamer/PycharmProjects/programst/PythonProjectmilo/DataFiles/age-between-50-and-69.csv')
+dfveryold = pd.read_csv('PythonProjectmilo/DataFiles/age-between-50-and-69.csv')
 dfveryold = dfveryold.dropna()
 dfveryold['Year'].astype('object')
 dfveryold['total_deaths'] = dfveryold.sum(axis=1, numeric_only=True)
